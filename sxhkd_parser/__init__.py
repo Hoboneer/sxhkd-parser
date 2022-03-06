d71dad4ff165a4656e8d6b3a7202afdc357bc923
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2022-03-06 14:18:46 +1300

    Add package docstring

--- sxhkd_parser/__init__.py
+++ sxhkd_parser/__init__.py
@@ -1,3 +1,10 @@
+"""Library for parsing Simple X Hotkey Daemon (sxhkd) keybinds.
+
+You should start with the `parser` module.
+
+Re-exports every member in all modules directly part of this package.  The
+`cli` subpackage as well as modules under it need to be imported explicitly.
+"""
 # mypy: implicit-reexport
 from ._package import *
 from .errors import *
